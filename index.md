e1cf10d56da08ee2bd6075fc4089a67e81efdb15
damda-not-found <michal19012003@gmail.com>
2024-03-04 01:22:49 +0100

    linki

--- index.md
+++ index.md
@@ -5,5 +5,20 @@ Popularne języki programowania
 # Języki programowania
 
 
-Ważne dla każdego programisty, który właśnie wchodzi na rynek pracy, jest wybranie odpowiedniego języka programowania. Dlatego prezentuję skróconą listę najbardziej popularnych języków programowania.  
-[lista](list.md)
+Ważne dla każdego programisty, który właśnie wchodzi na rynek pracy, jest wybranie odpowiedniego języka programowania. Dlatego prezentuję skróconą listę najbardziej popularnych języków programowania.
+# lista
+  
+[link do strony o Python](https://en.wikipedia.org/wiki/Python_(programming_language))
+## Python
+  
+[link do strony o C](https://www.geeksforgeeks.org/c-programming-language/)
+## C
+  
+[link do strony o C++](https://cplusplus.com/doc/tutorial/)
+## C++
+  
+[link do strony o Java](https://www.programiz.com/java-programming)
+## Java
+  
+[link do strony o C#](https://dotnet.microsoft.com/en-us/languages/csharp)
+## C#
--- scrap.py
+++ scrap.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from mdutils.mdutils import MdUtils
 from mdutils import Html
+from duckduckgo_search import DDGS
 
 URL = "https://www.tiobe.com/tiobe-index/"
 page = requests.get(URL)
@@ -15,10 +16,6 @@ resultTable = []
 for i in range(4, len(table.find_all("td"))):
     resultTable.append(table.find_all("td")[i].text)
 
-for i in range(7):
-    print(resultTable[i])
-
-
 listaNazwa = 'list'
 textLink = 'lista'
 
@@ -30,14 +27,24 @@ mainPage.new_header(level=1, title='Języki programowania')
 mainPage.new_paragraph("Ważne dla każdego programisty, który właśnie wchodzi na rynek pracy, jest wybranie odpowiedniego języka programowania. "
                      "Dlatego prezentuję skróconą listę najbardziej popularnych języków programowania.")
 
-mainPage.new_line(mainPage.new_inline_link(link=listaNazwa + '.md', text=textLink))
+#mainPage.new_line(mainPage.new_inline_link(link=listaNazwa + '.md', text=textLink))
+
+mainPage.new_header(level=1, title='lista')
+for i in range(5):
+    with DDGS() as ddgs:
+        results = ddgs.text(resultTable[7 * i] + " programming", max_results=5)
+        print(next(results))
+        mainPage.new_line(mainPage.new_inline_link(link=next(results)['href'], text='link do strony o ' + resultTable[7 * i]))
+
+    mainPage.new_header(level=2, title=resultTable[7 * i])
 mainPage.create_md_file()
 
-listPage = MdUtils(file_name=listaNazwa, title='Skrócona lista najbardziej popularnych języków programowania')
+#listPage = MdUtils(file_name=listaNazwa, title='Skrócona lista najbardziej popularnych języków programowania')
 
-listPage.new_header(level=1, title='lista')
+#listPage.new_header(level=1, title='lista')
 
-for i in range(5):
-    listPage.new_header(level=2, title=resultTable[7 * i])
+#for i in range(5):
+#    listPage.new_header(level=2, title=resultTable[7 * i])
 
-listPage.create_md_file()
+#listPage.create_md_file()
+
