c7ebb33caae970458ea4a9ffdf34b8d197e44864
damda-not-found <michal19012003@gmail.com>
2024-03-04 21:00:10 +0100

    update: opisy

--- index.md
+++ index.md
@@ -7,20 +7,30 @@ Ważne dla każdego programisty, który właśnie wchodzi na rynek pracy, jest w
 # lista
 
 ## Python
-  
-[link do strony o Python](https://en.wikipedia.org/wiki/Python_(programming_language))
+
+
+Z wyraźną przewagą, Python plasuje się na pierwszym miejscu najpopularniejszych języków programowania. Jest to od kilku lat najpopularniejszy język programowania i nic nie wskazuje na zmianę w przyszłych latach.  
+[link do strony wikipedii o Python](https://en.wikipedia.org/wiki/Python_(programming_language))
 ## C
-  
-[link do strony o C](https://en.wikipedia.org/wiki/C_(programming_language))
+
+
+Następnym na liście jest C. Pomimo, że C jest już ponad 50 letnim językiem programowania to nadal jest używany wśród programistów.  
+[link do strony wikipedii o C](https://en.wikipedia.org/wiki/C_(programming_language))
 ## C++
-  
-[link do strony o C++](https://en.wikipedia.org/wiki/C++)
+
+
+C++ znajduje się tuż za swoim przodkiem, C, na liście najbardziej popularnych języków programowania. Oprócz składni języka C, C++ oferuje również możliwość programowania obiektowego i metaprogramowania.  
+[link do strony wikipedii o C++](https://en.wikipedia.org/wiki/C++)
 ## Java
-  
-[link do strony o Java](https://en.wikipedia.org/wiki/Java_(programming_language))
+
+
+Java rzeczywiście jest językiem programowania. Należy do prywatnej firmy Oracle i jest tam dalej rozwijana.  
+[link do strony wikipedii o Java](https://en.wikipedia.org/wiki/Java_(programming_language))
 ## C#
-  
-[link do strony o C#](https://en.wikipedia.org/wiki/C_Sharp_(programming_language))  
+
+
+Tak jak Java, C# jest językiem czysto obiektowym i znajduje się w rękach prywatnej korporacji, Microsoftu. Dlatego użytkownicy Linuxa zazwyczaj nie programują w C#.  
+[link do strony wikipedii o C#](https://en.wikipedia.org/wiki/C_Sharp_(programming_language))  
 
  --- 
   
--- scrap.py
+++ scrap.py
@@ -22,19 +22,26 @@ textLink = 'lista'
 
 mainPage = MdUtils(file_name='index', title='Popularne języki programowania')
 
-#mainPage.new_header(level=1, title='Języki programowania')
-
 mainPage.new_paragraph("Ważne dla każdego programisty, który właśnie wchodzi na rynek pracy, jest wybranie odpowiedniego języka programowania. "
                      "Dlatego prezentuję skróconą listę najbardziej popularnych języków programowania.")
 
-#mainPage.new_line(mainPage.new_inline_link(link=listaNazwa + '.md', text=textLink))
+opisy = [
+"Z wyraźną przewagą, Python plasuje się na pierwszym miejscu najpopularniejszych języków programowania. "
+"Jest to od kilku lat najpopularniejszy język programowania i nic nie wskazuje na zmianę w przyszłych latach.",
+"Następnym na liście jest C. Pomimo, że C jest już ponad 50 letnim językiem programowania to nadal jest używany wśród programistów.",
+"C++ znajduje się tuż za swoim przodkiem, C, na liście najbardziej popularnych języków programowania. Oprócz składni języka C, C++ oferuje również możliwość programowania obiektowego"
+" i metaprogramowania.",
+"Java rzeczywiście jest językiem programowania. Należy do prywatnej firmy Oracle i jest tam dalej rozwijana.",
+"Tak jak Java, C# jest językiem czysto obiektowym i znajduje się w rękach prywatnej korporacji, Microsoftu. Dlatego użytkownicy Linuxa zazwyczaj nie programują w C#."
+]
 
 mainPage.new_header(level=1, title='lista')
 for i in range(5):
     mainPage.new_header(level=2, title=resultTable[7 * i])
     with DDGS() as ddgs:
         results = ddgs.text(resultTable[7 * i] + " programming wikipedia", max_results=5)
-        mainPage.new_line(mainPage.new_inline_link(link=next(results)['href'], text='link do strony o ' + resultTable[7 * i]))
+        mainPage.new_paragraph(opisy[i]);
+        mainPage.new_line(mainPage.new_inline_link(link=next(results)['href'], text='link do strony wikipedii o ' + resultTable[7 * i]))
 
 mainPage.new_line('\n --- \n')
 mainPage.new_line('źródło: ' + mainPage.new_inline_link(link='https://www.tiobe.com/tiobe-index/', text='Strona tiobe' ))
